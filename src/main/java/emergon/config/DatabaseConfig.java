2366ca3bb0ee2375e0c46028abcd54afc9ad1025
alkinoospap <alkispapakostas@hotmail.com>
2021-02-06 01:23:53 +0200

    r

--- src/main/java/emergon/config/DatabaseConfig.java
+++ src/main/java/emergon/config/DatabaseConfig.java
@@ -32,7 +32,7 @@ public class DatabaseConfig {
         datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         datasource.setUrl("jdbc:mysql://localhost:3306/sales?serverTimeZone=UTC");
         datasource.setUsername("root");
-        datasource.setPassword("");
+        datasource.setPassword("8VB9VNCMGNX6K");
         return datasource;
     }
     
